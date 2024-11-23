31cc463e34a9c670f5f31aec7ca998e19a967e71
ashish140399 <ashish140399@gmail.com>
2024-11-23 21:23:31 +0530

    added acci=ount page

--- src/views/AccountDashboard/components/MyProfile/index.tsx
+++ src/views/AccountDashboard/components/MyProfile/index.tsx
@@ -1,42 +1,19 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import { Button } from "@mui/material";
 import {
     ArrowTransformIcon,
-    ChevronLeftIcon,
-    ChevronRightIcon,
-    CustomDropdownIcon,
-    LayoutDistributeIcon,
-    LayoutGridIcon,
-    ThreeDotsIcon,
+    SuggestionCloseIcon,
+    UploadIcon,
 } from "../../../../assets/icons";
-import {
-    DashboardBalanceBG,
-    DashboardBalanceMobileBG,
-    GoldXRewardsBG,
-    GoldXRewardsMobileBG,
-    MiningPowerBG,
-    MiningPowerMobileBG,
-} from "../../../../assets/BG/BG";
-import { PieChart, Pie, Sector, Cell } from "recharts";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { createGlobalStyle } from "styled-components";
-
+import Dropzone, { Accept } from "react-dropzone";
 import { mobileBreakpoint } from "../../../../const";
-import SiteVariablesContext from "../../../../contexts/SiteVariablesContext";
 import ActionButton from "../../../../components/Buttons/ActionButton/ActionButton";
+import UploadImage from "../../../Minting/UploadImage";
 
 interface NFTsListProps {}
 
 const MyProfile: React.FC<NFTsListProps> = ({}) => {
-    const [layoutSelector, setLayoutSelector] = React.useState("grid");
-    const [chartContainersHeight, setChartContainersHeight] = useState(null);
-
-    const sliderRef = useRef(null);
-
     const [activeButtons, setActiveButtons] = useState("Edit Profile");
 
     const buttons = ["Edit Profile", "Withdraw", "Transaction", "Settings"];
@@ -45,9 +22,79 @@ const MyProfile: React.FC<NFTsListProps> = ({}) => {
         setActiveButtons(buttonName);
     };
 
+    const [previewUrl, setPreviewUrl] = useState("/images/common/cover.png");
+    const [userpreviewUrl, setUserpreviewUrl] = useState(
+        "/images/common/user.png"
+    );
+    const handleImgDrop = (acceptedFiles: any) => {
+        const file = acceptedFiles[0];
+
+        if (file) {
+            // Create a URL for the file
+            const fileUrl = URL.createObjectURL(file);
+
+            // Load the image to check its dimensions
+            const img = new Image();
+            img.onload = () => {
+                const { width, height } = img;
+
+                if (width >= 400 && height >= 400) {
+                    setPreviewUrl(fileUrl);
+                } else {
+                    alert("Image must be at least 400px by 400px");
+                    URL.revokeObjectURL(fileUrl); // Clean up the URL object
+                }
+            };
+            img.onerror = () => {
+                alert("Failed to load image");
+                URL.revokeObjectURL(fileUrl); // Clean up the URL object
+            };
+            img.src = fileUrl;
+        }
+    };
+    const handleUserImgDrop = (acceptedFiles: any) => {
+        const file = acceptedFiles[0];
+
+        if (file) {
+            // Create a URL for the file
+            const fileUrl = URL.createObjectURL(file);
+            console.log(fileUrl);
+            // Load the image to check its dimensions
+            const img = new Image();
+            img.onload = () => {
+                const { width, height } = img;
+
+                if (width >= 200 && height >= 200) {
+                    setUserpreviewUrl(fileUrl);
+                } else {
+                    alert("Image must be at least 400px by 400px");
+                    URL.revokeObjectURL(fileUrl); // Clean up the URL object
+                }
+            };
+            img.onerror = () => {
+                alert("Failed to load image");
+                URL.revokeObjectURL(fileUrl); // Clean up the URL object
+            };
+            img.src = fileUrl;
+        }
+    };
+
+    const acceptedFileTypes = {
+        "image/jpeg": [".jpg", ".jpeg"],
+        "image/png": [".png"],
+    };
     return (
-        <Layout>
-            <OptionsWrapper className="paddinglayoutx paddinglayouty">
+        <Layout className="paddinglayoutx paddinglayouty">
+            {" "}
+            <PageHeading>
+                <PageTitle>My Profile</PageTitle>
+                <PageDescription>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                </PageDescription>
+            </PageHeading>
+            <OptionsWrapper>
                 <SelectBoxWrapper>
                     {buttons.map((buttonName) => (
                         <MuiButtonStyle
@@ -63,8 +110,80 @@ const MyProfile: React.FC<NFTsListProps> = ({}) => {
                     ))}
                 </SelectBoxWrapper>
             </OptionsWrapper>
-            <ProfileWrapper></ProfileWrapper>
-            <DataRows className="paddinglayoutx">
+            <ProfileWrapper>
+                <Cover>
+                    <Dropzone onDrop={handleImgDrop} accept={acceptedFileTypes}>
+                        {({ getRootProps, getInputProps }: any) => (
+                            <div
+                                {...getRootProps()}
+                                style={{
+                                    height: "100%",
+                                    width: "100%",
+                                }}
+                            >
+                                {
+                                    <div className="imguploadedwrapper">
+                                        <div className="editbutton">
+                                            <img
+                                                src="/images/common/icons/edit.svg"
+                                                className="editicon"
+                                                alt=""
+                                            />
+                                            <input
+                                                {...getInputProps()}
+                                                multiple={false}
+                                            />
+                                        </div>
+                                        <img
+                                            src={previewUrl}
+                                            className="coverimg"
+                                            alt=""
+                                        />
+                                    </div>
+                                }
+                            </div>
+                        )}
+                    </Dropzone>
+                </Cover>
+                <UserImg>
+                    <Dropzone
+                        onDrop={handleUserImgDrop}
+                        accept={acceptedFileTypes}
+                    >
+                        {({ getRootProps, getInputProps }: any) => (
+                            <div
+                                {...getRootProps()}
+                                style={{
+                                    height: "100%",
+                                    width: "100%",
+                                }}
+                            >
+                                {
+                                    <div className="imguploadedwrapper">
+                                        <div className="editbutton">
+                                            <img
+                                                src="/images/common/icons/edit.svg"
+                                                className="editicon"
+                                                alt=""
+                                            />
+                                            <input
+                                                {...getInputProps()}
+                                                multiple={false}
+                                            />
+                                        </div>
+                                        <img
+                                            src={userpreviewUrl}
+                                            className="userimg"
+                                            alt=""
+                                        />
+                                    </div>
+                                }
+                            </div>
+                        )}
+                    </Dropzone>
+                </UserImg>
+            </ProfileWrapper>
+            <DataRows>
                 <LeftColumn>
                     <div className="title">General Information</div>
                     <div className="label">
@@ -99,7 +218,7 @@ const MyProfile: React.FC<NFTsListProps> = ({}) => {
                     </DataContainer>
                 </RightColumn>
             </DataRows>
-            <DataRows className="paddinglayoutx paddinglayouty">
+            <DataRows>
                 <LeftColumn>
                     <div className="title">Contact Information</div>
                     <div className="label">
@@ -142,7 +261,7 @@ const OptionsWrapper = styled.div`
     justify-content: space-between;
     margin-bottom: 40px;
     @media screen and (max-width: ${mobileBreakpoint}px) {
-        flex-direction: row-reverse;
+        flex-direction: row;
         overflow-x: auto;
         scrollbar-width: none;
         white-space: nowrap;
@@ -167,7 +286,7 @@ const MuiButtonStyle = styled(Button)`
 const SelectBoxWrapper = styled.div`
     @media screen and (max-width: ${mobileBreakpoint}px) {
         display: flex;
-        margin-left: 20px;
+        margin-left: 0px;
     }
 `;
 const LeftColumn = styled.div`
@@ -200,10 +319,9 @@ const DataRows = styled.div`
     justify-content: space-between;
     ${LeftColumn} {
         width: 50%;
-        .
     }
     ${RightColumn} {
-        width:  50%;
+        width: 50%;
         .imgwrapper {
             border-radius: 24px;
             overflow: hidden;
@@ -219,6 +337,14 @@ const DataRows = styled.div`
         margin-top: 40px;
     }
     @media screen and (max-width: ${mobileBreakpoint}px) {
+        flex-direction: column;
+        ${LeftColumn} {
+            width: 100%;
+            margin-bottom: 20px;
+        }
+        ${RightColumn} {
+            width: 100%;
+        }
     }
 `;
 const Layout = styled.section`
@@ -279,6 +405,85 @@ const TextAreaBox = styled.textarea`
     width: 100%;
     box-sizing: border-box;
 `;
+const Cover = styled.div`
+    height: 244px;
+    border-radius: 24px;
 
-const ProfileWrapper = styled.div``;
+    .coverimg {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        object-position: top;
+        border-radius: 24px;
+    }
+`;
+const UserImg = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    border: 6px solid #232323;
+    margin: auto;
+    margin-top: -100px;
+    .userimg {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        object-position: top;
+        border-radius: 50%;
+    }
+    .editbutton {
+        right: 20px !important;
+        transform: scale(0.8) !important;
+        transform-origin: bottom;
+        bottom: -10px !important;
+    }
+`;
+const ProfileWrapper = styled.div`
+    margin-bottom: 60px;
+    .imguploadedwrapper {
+        height: 100%;
+        width: 100%;
+        .editbutton {
+            position: absolute;
+            bottom: 0;
+            right: 4vw;
+            cursor: pointer;
+            transform: translateY(50%);
+            .editicon {
+            }
+        }
+    }
+    ${Cover},${UserImg} {
+        position: relative;
+    }
+`;
+const PageHeading = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-bottom: 30px;
+    position: relative;
+`;
+const PageTitle = styled.div`
+    color: var(--UI-White, var(--Typography-Primary-white, #fff));
+    font-feature-settings: "liga" off, "clig" off;
+    font-family: Conthrax;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 32px; /* 114.286% */
+    text-transform: uppercase;
+`;
+const PageDescription = styled.div`
+    color: var(--Text-Secondary, #cfcfcf);
+
+    /* P2/Regular */
+    font-family: Telegraf;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    margin-top: 10px;
+`;
+
 export default MyProfile;
